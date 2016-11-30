97c91e89f2ebed4d1633fd78a1afb6fc347c37a7
jodoudou <josua.modoux@gmail.com>
2016-11-30 14:48:49 +0100

    Ajout fct getValeurCase

--- src/Model/Jeu.java
+++ src/Model/Jeu.java
@@ -60,8 +60,15 @@ public class Jeu {
     }
     
     public void affiche(){
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(tabL.get(i).getCase(j).getVal().getV()+" ");
+            }
+            System.out.println("");
+        }
         
-        System.out.println("");
     }
-    
+    public String getValeurCase(int i, int j){
+        return Integer.toString(tabL.get(i).getCase(j).getVal().getV());
+    }
 }
