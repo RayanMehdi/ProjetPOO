c489a7fa160222fed968de4321b4b78768ac3784
easy <easy@192.168.1.25>
2016-12-17 10:37:42 +0100

    Ajout de du popup de victoire, suppression de la classe ProjetPOO.java inutile

--- src/View/MainView.java
+++ src/View/MainView.java
@@ -23,6 +23,7 @@ import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.Node;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
@@ -66,7 +67,7 @@ public class MainView extends Application {
     }
 
     @Override
-    public void start(Stage stage) throws Exception{
+    public void start(Stage stage) throws Exception {
         this.stage = stage;
         jeu = new Jeu("0 #6 4 3 #5 #1 9 2 8 8 3 #5 9 6 #2 #7 1 #4 1 2 #9 #8 7 4 5 3 6 4 #1 2 5 9 7 6 8 3 #5 #7 6 1 #3 #8 #2 4 9 3 9 8 #4 2 6 1 5 7 2 #8 3 7 1 9 4 6 #5 6 5 #7 2 #4 3 #8 9 1 9 4 1 #6 8 5 3 7 2");
         jeu.addObserver(new Observer() {
@@ -74,12 +75,12 @@ public class MainView extends Application {
             public void update(Observable o, Object arg) {
                 System.out.println("PB");
                 reload();
-                
+
             }
-          
+
         });
         initGrille();
-        
+
         initMenuListeners();
 
         stage.setTitle("Sudoku");
@@ -88,9 +89,9 @@ public class MainView extends Application {
         stage.show();
 
     }
-    
-    public void initGrille(){
-        
+
+    public void initGrille() {
+
         menu = new MenuBar();
         fichier = new Menu("Fichier");
         aide = new Menu("Aide");
@@ -100,7 +101,6 @@ public class MainView extends Application {
         fichier.getItems().addAll(save, load, exit);
         menu.getMenus().addAll(fichier, aide);
 
-       
         gPane = new GridPane();
         int column = 0;
         int row = 0;
@@ -111,8 +111,6 @@ public class MainView extends Application {
 
         scene = new Scene(border, 300, 300);
         scene.getStylesheets().add("style.css");
-
-        
 
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
@@ -132,11 +130,17 @@ public class MainView extends Application {
                                     //System.out.println("Début");
                                     jeu.getCase(GridPane.getRowIndex(tf), GridPane.getColumnIndex(tf)).update(Valeurs.fromString(newValue));
                                     //System.out.println("FIN");
-                                    
+
                                     // FIN
-                                    
-                                    if(jeu.fin())
+                                    if (jeu.fin()) {
+                                        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                                        alert.setTitle("Félicitation");
+                                        alert.setHeaderText(null);
+                                        alert.setContentText("Vous avez complété cette grille de Sudoku avec succès !");
+
+                                        alert.showAndWait();
                                         System.exit(0);
+                                    }
                                 }
                             }
 
@@ -144,11 +148,12 @@ public class MainView extends Application {
 
                     }
                     );
-                    caseNonBloquee c =(caseNonBloquee) jeu.getCase(i, j);
-                    if(c.getConflit())
+                    caseNonBloquee c = (caseNonBloquee) jeu.getCase(i, j);
+                    if (c.getConflit()) {
                         tf.getStyleClass().add("conflit");
-                    else
+                    } else {
                         tf.getStyleClass().add("nonBloque");
+                    }
                 }
 
                 tf.setPrefSize(scene.getWidth() / 9, scene.getHeight() / 9);
@@ -164,11 +169,11 @@ public class MainView extends Application {
 
         }
         border.setCenter(gPane);
-        
+
     }
-    
-    public void initMenuListeners(){
-         exit.setOnAction(new EventHandler<ActionEvent>() {
+
+    public void initMenuListeners() {
+        exit.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 System.exit(0);
@@ -190,18 +195,19 @@ public class MainView extends Application {
                 System.out.println(jeu);
             }
         });
-        
+
     }
 
-    public void reload(){
-        
+    public void reload() {
+
         initGrille();
         initMenuListeners();
-         stage.setTitle("Sudoku");
+        stage.setTitle("Sudoku");
         stage.setScene(scene);
 
         stage.show();
     }
+
     /**
      * @param args the command line arguments
      */
